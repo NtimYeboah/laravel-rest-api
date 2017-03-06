9e6fe65d3851400dfaa0e48f468ea67189717f5f
ntimobedyeboah <ntimyeboah.obed@callenssolutions.com>
2017-03-06 10:56:41 +0000

    Refactor user migrations and add user model factory

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ntimobedyeboah
+ * Date: 3/6/17
+ * Time: 10:54 AM
+ */
+use App\User;
+
+$factory->define(User::class, function(Faker\Generator $faker) {
+    return [
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'email' => $faker->safeEmail
+    ];
+});
